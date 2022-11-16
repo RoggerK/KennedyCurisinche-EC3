014ca35b491f5d0e62502767e969f3d676642aee
RoggerK <at76324897@idat.edu.pe>
2022-11-15 19:10:02 -0500

    init .

---
+++ src/main/java/rogger/guia/app/cita/Ec3KennedyCurisincheCitaApplication.java
@@ -0,0 +1,13 @@
+package rogger.guia.app.cita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3KennedyCurisincheCitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3KennedyCurisincheCitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rogger/guia/app/cita/Ec3KennedyCurisincheCitaApplicationTests.java
@@ -0,0 +1,13 @@
+package rogger.guia.app.cita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3KennedyCurisincheCitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
